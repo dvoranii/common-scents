2d1e676071e66a47300a67afe9d0fce47ddd9285
Ildi Dvorani <dvoranii@ildidev.com>
2025-11-19 15:27:11 -0500

    updated cors and created .env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      process.env.FRONTEND_URL,
+      "http://localhost:5173",
+      "http://localhost:4173",
+    ].filter(Boolean) as string[],
     credentials: true,
   })
 );
